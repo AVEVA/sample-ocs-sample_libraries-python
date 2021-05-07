a50fdf96fb42b44e0682cfc6670f2b03fd9ae65e
Doug Broad <dbroad@osisoft.com>
2021-05-07 13:35:55 -0400

    Add python_requires

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ocs_sample_library_preview",
-    version="0.2.3_preview",
-    author="OSIsoft",
-    license="Apache 2.0",
-    author_email="dendres@osisoft.com",
-    description="A preview of an OCS (OSIsoft Cloud Services) client library",
+    name='ocs_sample_library_preview',
+    version='0.2.3_preview',
+    author='OSIsoft',
+    license='Apache 2.0',
+    author_email='dendres@osisoft.com',
+    description='A preview of an OCS (OSIsoft Cloud Services) client library',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/osisoft/sample-ocs-sample_libraries-python",
+    long_description_content_type='text/markdown',
+    url='https://github.com/osisoft/sample-ocs-sample_libraries-python',
     packages=setuptools.find_packages(),
     install_requires=[
         'requests==2.25.1',
         'python-dateutil==2.8.1'
     ],
+    python_requires='>=3.9',
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
     ],
 )

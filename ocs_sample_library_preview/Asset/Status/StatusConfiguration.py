edd78edf1dbdc468101a814f0b7df543191408a6
Doug Broad <dbroad@osisoft.com>
2021-07-15 09:30:03 -0400

    Update assets REST API (#17)

---
+++ ocs_sample_library_preview/Asset/Status/StatusConfiguration.py
@@ -0,0 +1,72 @@
+import json
+
+from .StatusDefinitionType import StatusDefinitionType
+from .StatusMapping import StatusMapping
+
+
+class StatusConfiguration(object):
+    """OCS Asset Status Configuration definition"""
+
+    def __init__(self, definition_type: StatusDefinitionType = None, definition: StatusMapping = None):
+        """
+        :param definition_type: required
+        :param definition: required
+        """
+        self.DefinitionType = definition_type
+        self.Definition = definition
+
+    @property
+    def DefinitionType(self) -> StatusDefinitionType:
+        """
+        required
+        :return:
+        """
+        return self.__definition_type
+
+    @DefinitionType.setter
+    def DefinitionType(self, value: StatusDefinitionType):
+        """"
+        required
+        :param value:
+        :return:
+        """
+        self.__definition_type = value
+
+    @property
+    def Definition(self) -> StatusMapping:
+        """
+        required
+        :return:
+        """
+        return self.__definition
+
+    @Definition.setter
+    def Definition(self, value: str):
+        """
+        required
+        :param value:
+        :return:
+        """
+        self.__definition = value
+
+    def toJson(self):
+        return json.dumps(self.toDictionary())
+
+    def toDictionary(self):
+        # required properties
+        return {'DefinitionType': self.DefinitionType.value, 'Definition': self.Definition.toDictionary()}
+
+    @staticmethod
+    def fromJson(content: dict[str, str]):
+        result = StatusConfiguration()
+
+        if not content:
+            return result
+
+        if 'DefinitionType' in content:
+            result.DefinitionType = StatusDefinitionType[content['DefinitionType']]
+
+        if 'Definition' in content:
+            result.Definition = StatusMapping.fromJson(content['Definition'])
+
+        return result
